a96fd090ba57b7eb38f8e21d2e03b4341c39fbe0
Tina Huynh <tinamchuynh@gmail.com>
2023-02-07 09:17:41 -0800

    Auto stash before merge of "master" and "origin/master"

--- public/js/activity.js
+++ public/js/activity.js
@@ -2,7 +2,11 @@
  * Get repository info from GitHub API 
  */
 
+<<<<<<< Updated upstream
 $.getJSON('https://api.github.com/users/tmchuynh/repos?per_page=100', (data) => {
+=======
+$.getJSON('https://api.github.com/users/tmchuynh/repos?per_page=53', (data) => {
+>>>>>>> Stashed changes
     console.log(data);
 
     data.forEach((element) => {
--- public/js/projects.js
+++ public/js/projects.js
@@ -6,7 +6,7 @@ var obj = new Array();
  * Get repository info from GitHub API 
  */
 
-$.getJSON('https://api.github.com/users/tmchuynh/repos?per_page=100', (data) => {
+$.getJSON('https://api.github.com/users/tmchuynh/repos?per_page=53', (data) => {
     // console.log(data);
 
     data.forEach((element) => {
