ef9ef413e15d20c79ce02487d64fdc5b2d9f682e
Tina Huynh <tinamchuynh@gmail.com>
2023-02-09 11:05:14 -0800

    add screenshots into cards

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,137 +1,27 @@
 var project_container = document.getElementById('project-list');
 var cards = document.querySelectorAll(".cards");
-var module_container = document.querySelector('.offcanvas');
-var module_body = document.querySelector('.offcanvas-body');
-var module_title = document.querySelector('.offcanvas-title');
-var information = document.getElementsByClassName("large-container")[0];
 var obj = new Array();
-var el_data, languages_list;
+
 /**
  * Get repository info from GitHub API 
  */
 
 $.getJSON('https://api.github.com/users/tmchuynh/repos?per_page=53', (data) => {
-    console.log(data);
+    // console.log(data);
     data.forEach((element) => {
-        el_data = element;
 
 
         if (element.fork == false) {
             $.getJSON(element.languages_url, (data) => {
-                languages_list = Object.keys(data);
 
                 populate(element.name, element.pushed_at, Object.keys(data), element);
-
-                if (element.contents_url != "") {
-                    var array = element.contents_url.split("{");
-                    console.log(array[0]);
-                    $.getJSON(array[0], (htmlfiles) => {
-                        console.log(htmlfiles);
-                        if (htmlfiles.name == "README.md") {
-                            moduleCreator(element.name, Object.keys(data).element.html_url, element, htmlfiles.html_url)
-                        }
-                    });
-
-                }
             })
         }
     })
 })
 
-cards.forEach(element => {
-    element.addEventListener('click', () => {
-        moduleCreatorDefault(el_data.name, languages_list, el_data.html_url, el_data);
-    })
-});
-
-/*
- * Create module with project information
- */
-function moduleCreatorDefault(name, languages, url, element) {
-    module_title.innerHTML = name;
-    var link = document.createElement('a');
-    link.setAttribute("href", url);
-    link.innerHTML = url;
-    module_body.appendChild(link);
-
-    console.log(name, languages, url);
-    var name_parts = name.replace("-", " ");
-    var title = document.querySelector('.offcanvas-title');
-    name_parts = toTitleCase(name_parts);
-    title.innerHTML = name_parts;
-    var link = document.querySelector('.offcanvas-body a');
-    link.setAttribute("href", url);
-    link.innerHTML = String(url);
-    var large_container = document.createElement('div');
-    large_container.classList.add('large-container');
-    console.log(large_container);
-
-    // Create container for the screenshots of the project
-    var container = document.createElement('div');
-    container.classList.add("offcanvas-body");
-    var container_title = document.createElement('h3');
-    container_title.innerHTML = "Screenshots:";
-    container.appendChild(container_title);
-    var container_body = document.createElement('div');
-    var container_body_img = document.createElement('img');
-    var screenshot_url = "../public/screenshots/" + name + ".png";
-
-    if (checkFileExist(screenshot_url)) {
-        container_body_img.setAttribute("src", screenshot_url);
-        container_body.appendChild(container_body_img);
-        container.appendChild(container_body);
-        large_container.appendChild(container);
-    }
-
-    module_body.appendChild(large_container);
-}
-
-function moduleCreator(name, languages, url, element, readme_url) {
-    module_title.innerHTML = name;
-    var link = document.createElement('a');
-    link.setAttribute("href", url);
-    link.innerHTML = url;
-    module_body.appendChild(link);
-
-    // console.log(name, languages, url);
-    var name_parts = name.replace("-", " ");
-    var title = document.querySelector('.offcanvas-title');
-    name_parts = toTitleCase(name_parts);
-    title.innerHTML = name_parts;
-    var link = document.querySelector('.offcanvas-body a');
-    link.setAttribute("href", url);
-    link.innerHTML = String(url);
-    var large_container = document.createElement('div');
-    large_container.classList.add('large-container');
-    console.log(large_container);
 
 
-    // Create container for the screenshots of the project
-    var container = document.createElement('div');
-    container.classList.add("offcanvas-body");
-    var container_title = document.createElement('h3');
-    container_title.innerHTML = "Screenshots:";
-    container.appendChild(container_title);
-    var container_body = document.createElement('div');
-    var container_body_img = document.createElement('img');
-    var screenshot_url = "../public/screenshots/" + name + ".png";
-
-    if (checkFileExist(screenshot_url)) {
-        container_body_img.setAttribute("src", screenshot_url);
-        container_body.appendChild(container_body_img);
-        container.appendChild(container_body);
-        large_container.appendChild(container);
-    }
-
-    // Create container for the project README
-    var container_readme = document.createElement('div');
-    var container_readme_text = document.createElement('p');
-    container_readme_text.innerHTML = readme_url;
-    container_readme.appendChild(container_readme_text);
-    large_container.appendChild(container_readme);
-
-    module_body.appendChild(large_container);
-}
 
 /**
  * Populate repository project cards with language icons respectively
@@ -151,8 +41,6 @@ function populate(name, updated, languages, element) {
     card.setAttribute("data-bs-target", "#offcanvas");
     card.setAttribute("aria-controls", "offcanvasExample");
 
-    card.addEventListener("click", () => moduleCreator(name, languages, element.html_url));
-
     addLanguages(languages, card);
 
     var title = document.createElement("a");
@@ -171,6 +59,16 @@ function populate(name, updated, languages, element) {
     var day = date_0[2];
 
     // console.log(month, " ", day, " ", year)
+
+
+    if (checkFileExist("../public/screenshots/" + name + ".png")) {
+        var img = document.createElement("img");
+        img.classList.add("img-screenshot");
+        img.setAttribute("src", "../public/screenshots/" + name + ".png");
+        img.setAttribute("alt", name);
+        card.appendChild(img);
+    }
+    
 
     var subtitle = document.createElement("code");
     subtitle.classList.add("last-updated");
