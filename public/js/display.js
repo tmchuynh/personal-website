5ec50abe23c9074659d8e14fd2776989b463b01e
Tina Huynh <tinamchuynh@gmail.com>
2023-02-27 15:15:08 -0800

    update projects

--- public/js/display.js
+++ public/js/display.js
@@ -1,1 +1,6 @@
 console.log(window.localStorage.getItem('target'));
+
+var repo_name = window.localStorage.getItem('target').split("/");
+repo_name = repo_name[repo_name.length - 1];
+console.log(repo_name);
+
