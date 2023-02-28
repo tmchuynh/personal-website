223ca86e0979db197965eed08a436e21cb4c158c
Tina Huynh <tinamchuynh@gmail.com>
2023-02-28 11:54:44 -0800

    edit radius

--- public/js/rotate.js
+++ public/js/rotate.js
@@ -1,7 +1,7 @@
 // Author: Hoang Tran (https://fb.com/99.hoangtran)
 
 // You can change global variables here:
-var radius = 240; // how big of the radius
+var radius = 320; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
