97c0563d532df429cad73614bf7596482dc11ebb
tmchuynh <tinamchuynh@gmail.com>
2023-04-04 13:53:17 -0700

    🎨 style(activity.js): reformat code and add missing newlines The code has been reformatted to improve readability and consistency. Newlines have been added to the end of the file to conform to best practices.

--- public/js/activity.js
+++ public/js/activity.js
@@ -4,26 +4,26 @@
  * @param callback - The function to be executed when the DOM is ready.
  */
 function onReady(callback) {
-    var intervalId = window.setInterval(function() {
-      if (document.getElementsByTagName('body')[0] !== undefined) {
-        window.clearInterval(intervalId);
-        callback.call(this);
-      }
-    }, 9000);
-  }
-  
+  var intervalId = window.setInterval(function () {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 9000);
+}
+
 /**
  * If the visible parameter is true, then set the display style of the element with the given selector
  * to block, otherwise set it to none.
  * @param selector - The CSS selector for the element you want to show or hide.
  * @param visible - A boolean value that indicates whether the element should be visible or not.
  */
-  function setVisible(selector, visible) {
-    document.querySelector(selector).style.display = visible ? 'block' : 'none';
-  }
-  
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
 /* Calling the function onReady and passing in a function as an argument. */
-  onReady(function() {
-    setVisible('.page', true);
-    setVisible('#loading', false);
-  });
+onReady(function () {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
