8c9c006e41a01f9d6907d98ec07f166f2a1c3d80
tmchuynh <tinamchuynh@gmail.com>
2023-04-05 21:27:41 -0700

    🎨 style(about.html): add section-title div and h2 tags to section headers The section headers in the about.html file have been updated to use a consistent style. A new div with the class "section-title" has been added to each section, and the header text has been wrapped in an h2 tag. This improves the readability and visual consistency of the page.
    🎨 style(index.html): reformat text to improve readability
    The commit reformats the text in the index.html file to improve its readability. The changes include adding line breaks and adjusting the indentation of the text. The content of the file remains unchanged.
    
    🎨 style(index.html): format code and add divs for section titles
    This commit adds divs to group section titles and their respective content. It also formats the code to improve readability.
    
    🎨 style(mission.html): add section-title class to h2 elements
    The h2 elements in the mission.html file are now wrapped in a div with the class section-title. This improves the styling of the page by adding a consistent margin and padding to the section titles.
    
    🎨 style(carousel.css): adjust padding and margin of carousel elements
    The padding and margin of the carousel elements in carousel.css have been adjusted to improve the layout and spacing of the carousel.
    
    🎨 style(carousel.scss): adjust padding and margin of carousel elements
    The padding and margin of the carousel elements in carousel.scss have been adjusted to improve the layout and spacing of the carousel.
    
    🐛 fix(carousel.js): add responsive breakpoints to carousel
    The carousel in carousel.js has been updated to include responsive breakpoints for different screen sizes. This improves the layout and usability of the carousel on different devices.

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,25 +1,35 @@
 jQuery(document).ready(function($) {
-    "use strict";
-    //  TESTIMONIALS CAROUSEL HOOK
-    $('#customers-testimonials').owlCarousel({
-        loop: true,
-        center: true,
+  "use strict";
+  // TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 20,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 10
+      },
+      576: {
+        items: 2,
+        margin: 20
+      },
+      768: {
+        items: 3,
+        margin: 30
+      },
+      992: {
+        items: 4,
+        margin: 40
+      },
+      1200: {
         items: 5,
-        margin: 200,
-        autoplay: true,
-        dots:false,
-        autoplayTimeout: 8500,
-        smartSpeed: 450,
-        responsive: {
-          0: {
-            items: 1
-          },
-          768: {
-            items: 3
-          },
-          1170: {
-            items: 4
-          }
-        }
-    });
+        margin: 50
+      }
+    }
+  });
 });
