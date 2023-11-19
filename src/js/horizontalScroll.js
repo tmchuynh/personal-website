eeef70c5f8c608b46bd47c44e96cf61026c1c3e0
Tina Huynh <tinamchuynh@gmail.com>
2023-11-19 12:07:13 -0800

    delete 7 files

--- src/js/horizontalScroll.js
+++
@@ -1,6 +0,0 @@
-const scrollContainer = document.querySelector(".cards-wrapper");
-
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
